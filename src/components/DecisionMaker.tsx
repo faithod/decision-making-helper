db5a79eb703fe9265767c24dcf2f11c553c555e7
Faith <faithodularu@hotmail.com>
2022-02-11 11:25:42 +0000

    fixed dropdown list of weights

--- src/components/DecisionMaker.tsx
+++ src/components/DecisionMaker.tsx
@@ -114,6 +114,9 @@ export default function DecisionMaker() {
                   })
                 }
               >
+                <option selected disabled>
+                  select one
+                </option>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
