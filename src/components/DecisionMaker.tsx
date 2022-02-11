d3bfb54c8aa4c7322331c4405d291adb94bbde83
Faith <faithodularu@hotmail.com>
2022-02-11 18:58:29 +0000

    added return type to reducer function, added a score and winner state

--- src/components/DecisionMaker.tsx
+++ src/components/DecisionMaker.tsx
@@ -1,25 +1,96 @@
 import { useReducer } from "react";
+import calculateTotal from "../utils/calculateTotal";
+import getRatings from "../utils/getRatings";
+import getWeights from "../utils/getWeights";
 import ChoiceCard from "./ChoiceCard";
 
 //put in seperate folder, extract out what you can
 export interface attributeElement {
-  attributeName: string;
+  attributeName: string | number[];
   weight: undefined | number;
 }
 export interface choiceValuesElement {
-  choiceName: string;
+  choiceName: string | number[]; //change to just string
   ratings: (number | undefined)[];
+  score: undefined | number;
 }
 interface decisionState {
   choices: choiceValuesElement[];
   attributes: attributeElement[];
+  winner: undefined | string;
 }
 
 export interface action {
   type: string;
-  value: string;
-  index: number;
+  value: string | number[];
+  index?: number;
   attributeIndex?: number;
+}
+
+function reducer(state: decisionState, action: action): decisionState {
+  //is switch preffered to if/else?
+  if (action.type === "addChoice") {
+    return {
+      ...state,
+      choices: [...state.choices].map((el, i) =>
+        i === action.index ? { ...el, choiceName: action.value } : el
+      ),
+    };
+  } else if (action.type === "addAttribute") {
+    return {
+      ...state,
+      attributes: [...state.attributes].map((el, i) =>
+        i === action.index ? { ...el, attributeName: action.value } : el
+      ),
+    };
+  } else if (action.type === "addWeight") {
+    const chosenWeight = parseInt(
+      typeof action.value === "string" ? action.value : ""
+    );
+    return {
+      ...state,
+      attributes: [...state.attributes].map((el, i) =>
+        i === action.index ? { ...el, weight: chosenWeight } : el
+      ),
+    };
+  } else if (action.type === "addRating") {
+    return {
+      ...state,
+      choices: [...state.choices].map((el, i) => {
+        if (i === action.index) {
+          const chosenRating = parseInt(
+            typeof action.value === "string" ? action.value : ""
+          );
+          const ratings = el.ratings.map((rating, i) =>
+            i === action.attributeIndex ? chosenRating : rating
+          );
+          return { ...el, ratings: ratings };
+        } else {
+          return el;
+        }
+      }),
+    };
+  } else if (action.type === "addScores") {
+    return {
+      ...state,
+      choices: [...state.choices].map((el, i) => {
+        let scoreToAdd;
+        for (const score of action.value) {
+          let indexOfScore: number;
+          if (typeof action.value !== "string" && typeof score === "number") {
+            indexOfScore = action.value.indexOf(score);
+            if (i === indexOfScore) {
+              scoreToAdd = score;
+            }
+          }
+        }
+        return { ...el, score: scoreToAdd };
+      }),
+    };
+  } else {
+    // throw new Error("Unknown action type: " + action.type);
+    return state;
+  }
 }
 
 export default function DecisionMaker() {
@@ -28,14 +99,17 @@ export default function DecisionMaker() {
       {
         choiceName: "",
         ratings: [undefined, undefined, undefined],
+        score: undefined,
       },
       {
         choiceName: "",
         ratings: [undefined, undefined, undefined],
+        score: undefined,
       },
       {
         choiceName: "",
         ratings: [undefined, undefined, undefined],
+        score: undefined,
       },
     ],
     attributes: [
@@ -52,55 +126,37 @@ export default function DecisionMaker() {
         weight: undefined,
       },
     ],
+    winner: undefined,
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  function reducer(state: decisionState, action: action) {
-    let newState;
-    //is switch preffered to if/else?
-    if (action.type === "addChoice") {
-      newState = {
-        ...state,
-        choices: [...state.choices].map((el, i) =>
-          i === action.index ? { ...el, choiceName: action.value } : el
-        ),
-      };
-    } else if (action.type === "addAttribute") {
-      newState = {
-        ...state,
-        attributes: [...state.attributes].map((el, i) =>
-          i === action.index ? { ...el, attributeName: action.value } : el
-        ),
-      };
-    } else if (action.type === "addWeight") {
-      const chosenWeight = parseInt(action.value);
-      newState = {
-        ...state,
-        attributes: [...state.attributes].map((el, i) =>
-          i === action.index ? { ...el, weight: chosenWeight } : el
-        ),
-      };
-    } else if (action.type === "addRating") {
-      newState = {
-        ...state,
-        choices: [...state.choices].map((el, i) => {
-          if (i === action.index) {
-            const chosenRating = parseInt(action.value);
-            const ratings = el.ratings.map((rating, i) =>
-              i === action.attributeIndex ? chosenRating : rating
-            );
-            return { ...el, ratings: ratings };
-          } else {
-            return el;
-          }
-        }),
-      };
-    } else {
-      throw new Error();
+  function handleFindTheWinner() {
+    function calculateScores() {
+      const output = [];
+      const weights = getWeights(state.attributes);
+      console.log(weights);
+      const arrOfRatings = getRatings(state.choices);
+      console.log(arrOfRatings);
+      for (const rating of arrOfRatings) {
+        if (weights) {
+          const total = calculateTotal(weights, rating);
+          console.log(total);
+          output.push(total);
+        }
+      }
+      return output;
     }
-    return newState;
+    const arrayOfScores = calculateScores();
+    const largestScore = arrayOfScores.sort((a, b) => a - b)[0];
+    const indexOfLargestScore = arrayOfScores.indexOf(largestScore);
+    const winner = state.choices.filter(
+      (el: choiceValuesElement, i: number) => i === indexOfLargestScore
+    )[0].choiceName;
+
+    dispatch({ type: "addScores", value: arrayOfScores });
   }
+  //   console.log(handleFindTheWinner());
   //placeholders: brownies, pizza, sandwhich
   console.log(state);
   return (
@@ -112,7 +168,7 @@ export default function DecisionMaker() {
             <input
               key={i}
               type="text"
-              value={el.choiceName}
+              value={typeof el.choiceName === "string" ? el.choiceName : ""}
               onChange={(e) =>
                 dispatch({ type: "addChoice", value: e.target.value, index: i })
               }
@@ -125,7 +181,9 @@ export default function DecisionMaker() {
             <div key={i}>
               <input
                 type="text"
-                value={el.attributeName}
+                value={
+                  typeof el.attributeName === "string" ? el.attributeName : ""
+                }
                 onChange={(e) =>
                   dispatch({
                     type: "addAttribute",
@@ -168,6 +226,8 @@ export default function DecisionMaker() {
           dispatch={dispatch}
         />
       ))}
+      <button onClick={handleFindTheWinner}>Find the Winner</button>
+      <p>Winner:</p>
     </>
   );
 }
