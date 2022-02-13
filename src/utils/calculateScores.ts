3398a281fb0a6668137a1abc5f80880888894002
Faith <faithodularu@hotmail.com>
2022-02-13 17:24:25 +0000

    formatted with prettier

--- src/utils/calculateScores.ts
+++ src/utils/calculateScores.ts
@@ -4,15 +4,18 @@ import calculateTotal from "../utils/calculateTotal";
 import getRatings from "../utils/getRatings";
 import getWeights from "../utils/getWeights";
 
-export default function calculateScores(attributes: AttributeElement[], choices: ChoiceValuesElement[]) {
-    const output = [];
-    const weights = getWeights(attributes);
-    const arrOfRatings = getRatings(choices);
-    for (const rating of arrOfRatings) {
-      if (weights) {
-        const total = calculateTotal(weights, rating);
-        output.push(total);
-      }
+export default function calculateScores(
+  attributes: AttributeElement[],
+  choices: ChoiceValuesElement[]
+) {
+  const output = [];
+  const weights = getWeights(attributes);
+  const arrOfRatings = getRatings(choices);
+  for (const rating of arrOfRatings) {
+    if (weights) {
+      const total = calculateTotal(weights, rating);
+      output.push(total);
     }
-    return output;
   }
+  return output;
+}
